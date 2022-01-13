41a703b20adec71e7f0c385c7fd68f5fa89a5dd1
Naveenkant93 <navi.vap2@gmail.com>
2022-01-13 10:40:19 +0530

    minor fixes

--- app/src/main/java/com/soft/credit911/NetworkUtils/APIConstants.java
+++ app/src/main/java/com/soft/credit911/NetworkUtils/APIConstants.java
@@ -2,7 +2,7 @@ package com.soft.credit911.NetworkUtils;
 
 public interface APIConstants {
 //    String BASE_URL = "https://911credit.dev/";
-     String BASE_URL = "https://app.creditmerlin.com/";
+     String BASE_URL = "http://app.creditmerlin.com/";
 
     String RESET_PASSWORD_API = "api/reset-password?";
     String GET_USER_INFO = "api/get-user-info";
